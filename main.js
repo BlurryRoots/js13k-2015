c20c8be3795431155f1a9d61883fa5211dedbdc3
blurryroots <blurryroots@posteo.de>
2015-09-01 21:55:46 +0200

    code structure, example shapes

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ Basis for AppLauncher and Randomizer by ooflorent at https://github.com/oofloren
 
 var Debug = {
   log: function (msg) {
-    console.log (msg);
+    //console.log (msg);
+    var logger = document.querySelector ('#log');
+    logger.textContent = msg;
   }
 }
 
-var AppLauncher = function () {
+var AppLauncher = function (canvas) {
   // Holds last iteration timestamp.
   var time = 0;
+  var ctx = canvas.getContext ('2d');
 
   /**
    * Calls `fn` on next frame.
@@ -47,9 +50,11 @@ var AppLauncher = function () {
      * @return {int} The request ID
      * @api public
      */
-    start: function (fn) {
+    start: function (game) {
       return callNextFrame (function tick (elapsed) {
-        fn (elapsed);
+        game.update (elapsed);
+        game.render (ctx);
+        // call again later
         callNextFrame (tick);
       });
     },
@@ -132,14 +137,17 @@ var Randomizer = function (seed) {
   return rng;
 };
 
-var Superformular = function (m, a, b, n1, n2, n3) {
+var Superformular = function (m, n1, n2, n3, a, b) {
+  if (!a || a <= 0) a = 1;
+  if (!b || b <= 0) b = 1;
+
   return {
     m: m,
-    a: a,
-    b: b,
     n1: n1,
     n2: n2,
     n3: n3,
+    a: a, // stretch in -x
+    b: b, // stretch in +x
     calculate_point: function (angle) {
       var rpart = (this.m * angle) / 4;
       var apart = Math.abs (Math.cos (rpart) / this.a);
@@ -156,48 +164,188 @@ var Superformular = function (m, a, b, n1, n2, n3) {
       };
     },
   }
+};
+
+function linearInterpolation (from, to, currentTime, intervalLength) {
+  return ((to - from) * (currentTime / intervalLength)) + from;
 }
 
-var canvas = document.querySelector ('#game');
-var ctx = canvas.getContext ('2d');
-var rand = Randomizer (1337);
+var Color = function (r, g, b) {
+  function componentToHexString (comp) {
+    var str = comp.toString (16);
 
-function renderSuperFormular (elapsed) {
-  var sf = Superformular (3, 1, 1, 2, 8, 3);
+    return str.length == 1
+      ? "0" + str
+      : str
+      ;
+  }
+  return {
+    r: r || 0,
+    g: g || 0,
+    b: b || 0,
 
+    toHex: function () {
+      return "#"
+        + componentToHexString (this.r)
+        + componentToHexString (this.g)
+        + componentToHexString (this.b)
+        ;
+    },
+
+    interpolate: function (other, currentTime, intervalLength) {
+      return Color (
+        Math.floor (linearInterpolation (this.r, other.r, currentTime, intervalLength)),
+        Math.floor (linearInterpolation (this.g, other.g, currentTime, intervalLength)),
+        Math.floor (linearInterpolation (this.b, other.b, currentTime, intervalLength))
+      );
+    }
+  }
+}
+
+var SuperformularCollection = function (transitionTime) {
+  var collection = [];
+  var currentFormularIndex = 0;
+  var elapsedTime = 0;
   var vertices = [];
   var res = 360;
-  for (var i = 1; i <= res; ++i) {
-    var radians = (i / res) * (2 * Math.PI);
-    var point = sf.calculate_point (radians, 1);
+  var fillColor = Color (0, 0, 0);
+  var zoom = 150;
 
-    vertices.push (point);
+  return {
+    add: function (formular, color) {
+      collection.push ({
+        formular: formular,
+        color: color,
+      });
+    },
+    each: function (callback) {
+      collection.forEach (function (v) {
+        callback (v.formular, v.color);
+      });
+    },
+    update: function (dt) {
+      elapsedTime += dt;
+
+      if (elapsedTime > transitionTime) {
+        elapsedTime = 0;
+
+        var nextIndex = currentFormularIndex + 1;
+        currentFormularIndex = nextIndex >= collection.length
+          ? 0
+          : nextIndex
+          ;
+      }
+
+      var tansitionFormularIndex = currentFormularIndex + 1;
+      if (tansitionFormularIndex >= collection.length) {
+        tansitionFormularIndex = 0;
+      }
+
+      var sf1 = collection[currentFormularIndex].formular;
+      var sf2 = collection[tansitionFormularIndex].formular;
+
+      vertices = [];
+      for (var i = 1; i <= res; ++i) {
+        var radians = (i / res) * (2 * Math.PI);
+        var point1 = sf1.calculate_point (radians, 1);
+        var point2 = sf2.calculate_point (radians, 1);
+
+        vertices.push ({
+          x: linearInterpolation (point1.x, point2.x, elapsedTime, transitionTime),
+          y: linearInterpolation (point1.y, point2.y, elapsedTime, transitionTime),
+        });
+      }
+
+      var sf1color = collection[currentFormularIndex].color;
+      var sf2color = collection[tansitionFormularIndex].color;
+      fillColor = sf1color.interpolate (sf2color, elapsedTime, transitionTime);
+    },
+    draw: function (ctx) {
+      ctx.save ();
+      ctx.translate (canvas.width / 2, canvas.height / 2);
+      ctx.scale (zoom, zoom);
+      ctx.beginPath ();
+      vertices.forEach (function (v) {
+        ctx.lineTo (v.x, v.y);
+      });
+      ctx.closePath ();
+      ctx.restore ();
+
+      ctx.fillStyle = fillColor.toHex ();
+      ctx.fill ();
+    },
   }
-
-  var zoom = 100;
-  var offset = {
-    x: 400,
-    y: 300,
-  };
-
-  ctx.beginPath ();
-  var isFirst = true;
-  vertices.forEach (function (v) {
-    var x = v.x * zoom + offset.x;
-    var y = v.y * zoom + offset.y;
-
-    if (isFirst) {
-      isFirst = false;
-      ctx.moveTo (x, y);
-    }
-    else {
-      ctx.lineTo (x, y);
-    }
-  });
-  ctx.closePath ();
-  ctx.fillStyle = "#FFDC00";
-  ctx.fill ();
 }
 
-var app = AppLauncher ();
-app.start (renderSuperFormular);
+
+
+var Game = function () {
+  var rand = Randomizer (1337);
+  var sfc = SuperformularCollection (4.2);
+
+  function pickColor () {
+    return Color (
+      rand.range (0, 256),
+      rand.range (0, 256),
+      rand.range (0, 256)
+    );
+  }
+
+  var shapeValues = [
+    /*[[3, 2, 8, 3], Color (128, 0, 0)],
+    [[10, 10, 8, 7], Color (0, 128, 0)],
+    [[23, 10, 8, 7], Color (0, 0, 128)],
+    [[1, 1, 1, 1], pickColor ()],
+    [[23, 23, 23, 23], pickColor ()], */
+    [[3, 5, 18, 18], pickColor ()],
+    [[6, 20, 7, 18], pickColor ()],
+    [[4, 2, 4, 13], pickColor ()],
+    [[7, 3, 4, 17], pickColor ()],
+    [[7, 3, 6, 6], pickColor ()],
+    [[3, 3, 14, 2], pickColor ()],
+    [[19, 9, 14, 11], pickColor ()],
+    [[12, 15, 20, 3], pickColor ()],
+    [[8, 1, 1, 8], pickColor ()],
+    [[8, 1, 5, 8], pickColor ()],
+    [[8, 3, 4, 3], pickColor ()],
+    [[8, 7, 8, 2], pickColor ()],
+    [[5, 2, 6, 6], pickColor ()],
+    [[6, 1, 1, 6], pickColor ()],
+    [[6, 1, 7, 8], pickColor ()],
+    [[7, 2, 8, 4], pickColor ()],
+    [[3, 2, 8, 3], pickColor ()],
+    [[3, 6, 6, 6], pickColor ()],
+    [[4, 1, 7, 8], pickColor ()],
+    [[4, 4, 7, 7], pickColor ()],
+    [[2, 2, 2, 2], pickColor ()],
+    [[2, 1, 1, 1], pickColor ()],
+    [[2, 1, 4, 8], pickColor ()],
+    [[3, 2, 5, 7], pickColor ()],
+  ];
+
+  shapeValues.forEach (function (entry) {
+    var m = entry[0][0];
+    var n1 = entry[0][1];
+    var n2 = entry[0][2];
+    var n3 = entry[0][3];
+    var color = entry[1];
+
+    sfc.add (Superformular (m, n1, n2, n3), color);
+  });
+
+  return {
+    update: function (dt) {
+      sfc.update (dt);
+    },
+    render: function (ctx) {
+      ctx.clearRect (0, 0, canvas.width, canvas.height);
+
+      sfc.draw (ctx);
+    },
+  }
+};
+
+var canvas = document.querySelector ('#game');
+var launcher = AppLauncher (canvas);
+
+launcher.start (Game ());

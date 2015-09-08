4d7ed402c881278cd9db446784d2129d96b1ba97
blurryroots <blurryroots@posteo.de>
2015-09-08 03:03:56 +0200

    give canvas to game, text stuff

--- src/game.js
+++ src/game.js
@@ -1,10 +1,13 @@
 module.exports = (function () {
   Util = require ('./util');
+  Vector2 = require ('./vector2');
 
   var _text;
+  var _clickPos;
 
-  function GameClass () {
+  function GameClass (canvas) {
     _text = 'what the shit?';
+    _canvas = canvas;
   }
 
   GameClass.prototype.onupdate = function (dt) {
@@ -12,9 +15,18 @@ module.exports = (function () {
   };
 
   GameClass.prototype.onrender = function (ctx) {
-    var height = Util.fontHeight (ctx);
+    ctx.clearRect (0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    ctx.fillText (_text, 10, height);
+    if (_clickPos) {
+      var p = Util.clientToCanvasPosition (_canvas, _clickPos);
+      ctx.fillText (_text, p.x, p.y);
+    }
+  };
+
+  GameClass.prototype.onmousedown = function (e) {
+    console.log (e);
+
+    _clickPos = new Vector2 (e.clientX, e.clientY);
   };
 
   return GameClass;
